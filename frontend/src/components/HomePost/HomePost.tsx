99d5a47d755107ae028e42251f422decb06960b1
kxrn0 <kxrp@protonmail.com>
2024-02-09 11:58:10 -0600

    I wrote a bit more of the frontend

--- frontend/src/components/HomePost/HomePost.tsx
+++ frontend/src/components/HomePost/HomePost.tsx
@@ -1,4 +1,5 @@
 import { useDBContext } from "../../context/db.tsx";
+import { baseUrl } from "../../data.ts";
 import { PostType } from "../../types.ts";
 import SCHomePost from "./HomePost.styled.tsx";
 import { IconArrowUp, IconArrowDown } from "@tabler/icons-solidjs";
@@ -10,11 +11,44 @@ type Props = {
 export default function HomePost(props: Props) {
   const dbStore = useDBContext();
 
-  function upvote() {
-    console.log(props.post);
-  }
+  async function vote(event: { target: HTMLElement }) {
+    const value = Number(event.target.dataset.value);
+    let upvote, downvote;
 
-  function downvote() {}
+    if (value === 1) {
+      const hasUpvoted = dbStore
+        .upvoted()
+        .some((post) => post.id === props.post._id);
+
+      if (hasUpvoted) upvote = -1;
+      else upvote = 1;
+
+      downvote = 0;
+    } else {
+      const hasDownvoted = dbStore
+        .downvoted()
+        .some((post) => post.id === props.post._id);
+
+      if (hasDownvoted) downvote = -1;
+      else downvote = 1;
+
+      upvote = 0;
+    }
+
+    try {
+      const response = await fetch(`${baseUrl}/api/posts/${props.post._id}`, {
+        method: "POST",
+        body: JSON.stringify({ upvote, downvote }),
+      });
+      const json = await response.json();
+
+      if (json.newToken) dbStore.setToken(json.newToken);
+      
+      
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   return (
     <SCHomePost>
@@ -22,20 +56,22 @@ export default function HomePost(props: Props) {
         <a href={`/solid_blog/posts/${props.post._id}`}>{props.post.title}</a>
         <div class="score">
           <button
+            data-value="1"
             classList={{
               upvote: true,
               active: dbStore
                 .upvoted()
                 .some((post) => post.id === props.post._id),
             }}
-            onclick={upvote}
+            onclick={vote}
           >
             <IconArrowUp />
             <p>{props.post.upvotes}</p>
           </button>
           <div class="separator"></div>
           <button
-            onClick={downvote}
+            data-value="-1"
+            onClick={vote}
             classList={{
               downvote: true,
               active: dbStore
